7e61673cb8bbf1dc01698405f4c1b86a91a9a7ec
joinhu <karthikmadamshetty222@gmail.com>
2015-11-02 12:47:12 +0530

    dfgr

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphismdemo
+{
+    class Animal
+    {
+        public virtual void Eat()
+        {
+            Console.WriteLine("The animal ate!");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("The dog ate!");
+        }
+    }
+
+    class Bird : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("The bird ate!");
+        }
+    }
+
+    class Fish : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("The fish ate!");
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            Dog myDog = new Dog();
+            Bird myBird = new Bird();
+            Fish myFish = new Fish();
+            Animal myAnimal = new Animal();
+
+            myAnimal.Eat();
+
+            myAnimal = myDog;
+            myAnimal.Eat();
+            myAnimal = myBird;
+            myAnimal.Eat();
+            myAnimal = myFish;
+            myAnimal.Eat();
+        }
+    }
+}
